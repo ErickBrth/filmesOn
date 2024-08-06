9ae57525e438096a8a18b0a6acc6d3574cad7d3c
ErickBrth <erickaraken@gmail.com>
2024-08-06 11:43:28 -0300

    Initial Commit

---
+++ desafio/src/main/java/com/ubots/desafio/FilmesOnApplication.java
@@ -0,0 +1,13 @@
+package com.ubots.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmesOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmesOnApplication.class, args);
+	}
+
+}
---
+++ desafio/src/test/java/com/ubots/desafio/FilmesOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubots.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmesOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
