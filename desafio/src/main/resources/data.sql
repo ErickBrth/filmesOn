ef255b5dbe67b51d80da2168a0b95593dbe62185
ErickBrth <erickaraken@gmail.com>
2024-08-06 17:32:02 -0300

    feat: add database and swagger

---
+++ desafio/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO movie (title, description, reviewed, rating) VALUES ('Inception', 'A mind-bending thriller', false, 0);
+INSERT INTO movie (title, description, reviewed, rating) VALUES ('Interstellar', 'A journey through space and time', false, 0);
